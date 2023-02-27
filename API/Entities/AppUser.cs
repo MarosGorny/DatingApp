ee390666e9ca7282ee7e180605ab1d4ed08e15aa
Maroš Gorný <maros.gorny@gmail.com>
2023-02-27 21:41:05 +0100

    Add project files.

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //[Key] if Id would have another name
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
