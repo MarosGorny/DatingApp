8ae1539be0b420d744b263695ba99256c783363e
Maroš Gorný <maros.gorny@gmail.com>
2023-03-06 21:21:00 +0100

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -35,9 +35,7 @@ namespace API.Entities
 
         public List<Photo> Photos { get; set; } = new();
 
-        // public int GetAge() 
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public List<UserLike> LikedByUsers {get; set;}
+        public List<UserLike> LikedUsers { get; set; }
     }
 }
